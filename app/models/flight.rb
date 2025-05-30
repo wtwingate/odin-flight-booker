5f8a385216c3a83a76ca8563e066f3fc3b4e7455
William Wingate <wtwingate@gmail.com>
2025-05-30 16:28:06 -0400

    Add custom validations for Flight model

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,9 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
 
+  validate :arrival_airport_cannot_be_departure_airport,
+    :arrival_time_must_be_after_departure_time
+
   validates :departure_time, presence: true
   validates :arrival_time, presence: true
 
@@ -13,5 +16,19 @@ class Flight < ApplicationRecord
   def self.dates
     departure_times = Flight.pluck(:departure_time)
     departure_times.map(&:to_date).uniq
+  end
+
+  private
+
+  def arrival_airport_cannot_be_departure_airport
+    if departure_airport == arrival_airport
+      errors.add(:arrival_airport, "cannot be the same as departure airport")
+    end
+  end
+
+  def arrival_time_must_be_after_departure_time
+    if arrival_time <= departure_time
+      errors.add(:arrival_time, "must be after departure time")
+    end
   end
 end
