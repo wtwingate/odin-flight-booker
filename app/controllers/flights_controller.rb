41100cf66472054c158b8a34f6e402fe65bf1b83
William Wingate <wtwingate@gmail.com>
2025-05-30 22:32:26 -0400

    Filter and display flights by search params

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,9 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @flights = Flight.order(departure_time: :asc)
+    @flights = @flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
+    @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+    @flights = @flights.on_date(Date.parse(params[:date])) if params[:date].present?
   end
 
   def show
