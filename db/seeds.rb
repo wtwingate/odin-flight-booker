3009ef429d7a1e73432553138f96f69fd475abb3
William Wingate <wtwingate@gmail.com>
2025-05-30 22:44:15 -0400

    Add more flights to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,6 +25,7 @@ airport_codes.each do |code|
   Airport.find_or_create_by!(code: code)
 end
 
+# Thanks ChatGPT lol
 flight_details = [
   {
     departure_airport: "CLT",
@@ -85,6 +86,246 @@ flight_details = [
     arrival_airport: "ORD",
     departure_time: "2025-06-03T06:15:00-07:00",
     arrival_time: "2025-06-03T10:35:00-05:00"
+  },
+  {
+    departure_airport: "ATL",
+    arrival_airport: "MCO",
+    departure_time: "2025-05-30T07:15:00-04:00",
+    arrival_time: "2025-05-30T08:45:00-04:00"
+  },
+  {
+    departure_airport: "MCO",
+    arrival_airport: "ATL",
+    departure_time: "2025-05-30T10:30:00-04:00",
+    arrival_time: "2025-05-30T12:00:00-04:00"
+  },
+  {
+    departure_airport: "DFW",
+    arrival_airport: "DEN",
+    departure_time: "2025-05-30T09:45:00-05:00",
+    arrival_time: "2025-05-30T10:55:00-06:00"
+  },
+  {
+    departure_airport: "ORD",
+    arrival_airport: "DFW",
+    departure_time: "2025-05-30T14:20:00-05:00",
+    arrival_time: "2025-05-30T16:40:00-05:00"
+  },
+  {
+    departure_airport: "SEA",
+    arrival_airport: "LAX",
+    departure_time: "2025-05-30T16:30:00-07:00",
+    arrival_time: "2025-05-30T19:10:00-07:00"
+  },
+  {
+    departure_airport: "PHX",
+    arrival_airport: "SEA",
+    departure_time: "2025-05-31T06:45:00-07:00",
+    arrival_time: "2025-05-31T09:30:00-07:00"
+  },
+  {
+    departure_airport: "LAX",
+    arrival_airport: "LAS",
+    departure_time: "2025-05-31T11:20:00-07:00",
+    arrival_time: "2025-05-31T12:25:00-07:00"
+  },
+  {
+    departure_airport: "CLT",
+    arrival_airport: "MCO",
+    departure_time: "2025-05-31T13:40:00-04:00",
+    arrival_time: "2025-05-31T15:20:00-04:00"
+  },
+  {
+    departure_airport: "DEN",
+    arrival_airport: "ORD",
+    departure_time: "2025-05-31T15:15:00-06:00",
+    arrival_time: "2025-05-31T18:30:00-05:00"
+  },
+  {
+    departure_airport: "LAS",
+    arrival_airport: "PHX",
+    departure_time: "2025-05-31T18:50:00-07:00",
+    arrival_time: "2025-05-31T19:55:00-07:00"
+  },
+  {
+    departure_airport: "ORD",
+    arrival_airport: "ATL",
+    departure_time: "2025-06-01T05:30:00-05:00",
+    arrival_time: "2025-06-01T08:15:00-04:00"
+  },
+  {
+    departure_airport: "DFW",
+    arrival_airport: "MCO",
+    departure_time: "2025-06-01T07:45:00-05:00",
+    arrival_time: "2025-06-01T11:10:00-04:00"
+  },
+  {
+    departure_airport: "LAX",
+    arrival_airport: "ATL",
+    departure_time: "2025-06-01T08:15:00-07:00",
+    arrival_time: "2025-06-01T15:40:00-04:00"
+  },
+  {
+    departure_airport: "SEA",
+    arrival_airport: "DEN",
+    departure_time: "2025-06-01T10:20:00-07:00",
+    arrival_time: "2025-06-01T13:50:00-06:00"
+  },
+  {
+    departure_airport: "ATL",
+    arrival_airport: "ORD",
+    departure_time: "2025-06-01T14:30:00-04:00",
+    arrival_time: "2025-06-01T15:25:00-05:00"
+  },
+  {
+    departure_airport: "MCO",
+    arrival_airport: "DFW",
+    departure_time: "2025-06-02T06:25:00-04:00",
+    arrival_time: "2025-06-02T08:15:00-05:00"
+  },
+  {
+    departure_airport: "LAS",
+    arrival_airport: "SEA",
+    departure_time: "2025-06-02T09:30:00-07:00",
+    arrival_time: "2025-06-02T12:10:00-07:00"
+  },
+  {
+    departure_airport: "PHX",
+    arrival_airport: "LAX",
+    departure_time: "2025-06-02T11:40:00-07:00",
+    arrival_time: "2025-06-02T12:50:00-07:00"
+  },
+  {
+    departure_airport: "CLT",
+    arrival_airport: "DFW",
+    departure_time: "2025-06-02T13:15:00-04:00",
+    arrival_time: "2025-06-02T15:00:00-05:00"
+  },
+  {
+    departure_airport: "DEN",
+    arrival_airport: "LAS",
+    departure_time: "2025-06-02T16:45:00-06:00",
+    arrival_time: "2025-06-02T17:35:00-07:00"
+  },
+  {
+    departure_airport: "ATL",
+    arrival_airport: "CLT",
+    departure_time: "2025-06-03T07:10:00-04:00",
+    arrival_time: "2025-06-03T08:15:00-04:00"
+  },
+  {
+    departure_airport: "LAX",
+    arrival_airport: "PHX",
+    departure_time: "2025-06-03T09:25:00-07:00",
+    arrival_time: "2025-06-03T10:40:00-07:00"
+  },
+  {
+    departure_airport: "ORD",
+    arrival_airport: "MCO",
+    departure_time: "2025-06-03T10:50:00-05:00",
+    arrival_time: "2025-06-03T14:35:00-04:00"
+  },
+  {
+    departure_airport: "SEA",
+    arrival_airport: "ORD",
+    departure_time: "2025-06-03T12:15:00-07:00",
+    arrival_time: "2025-06-03T18:05:00-05:00"
+  },
+  {
+    departure_airport: "DFW",
+    arrival_airport: "ATL",
+    departure_time: "2025-06-03T15:30:00-05:00",
+    arrival_time: "2025-06-03T18:35:00-04:00"
+  },
+  {
+    departure_airport: "MCO",
+    arrival_airport: "LAX",
+    departure_time: "2025-06-04T06:20:00-04:00",
+    arrival_time: "2025-06-04T08:45:00-07:00"
+  },
+  {
+    departure_airport: "DEN",
+    arrival_airport: "DFW",
+    departure_time: "2025-06-04T08:35:00-06:00",
+    arrival_time: "2025-06-04T11:30:00-05:00"
+  },
+  {
+    departure_airport: "LAS",
+    arrival_airport: "DEN",
+    departure_time: "2025-06-04T10:40:00-07:00",
+    arrival_time: "2025-06-04T13:45:00-06:00"
+  },
+  {
+    departure_airport: "PHX",
+    arrival_airport: "DFW",
+    departure_time: "2025-06-04T13:20:00-07:00",
+    arrival_time: "2025-06-04T16:30:00-05:00"
+  },
+  {
+    departure_airport: "CLT",
+    arrival_airport: "ORD",
+    departure_time: "2025-06-04T16:15:00-04:00",
+    arrival_time: "2025-06-04T17:25:00-05:00"
+  },
+  {
+    departure_airport: "ATL",
+    arrival_airport: "SEA",
+    departure_time: "2025-06-05T07:40:00-04:00",
+    arrival_time: "2025-06-05T10:20:00-07:00"
+  },
+  {
+    departure_airport: "LAX",
+    arrival_airport: "MCO",
+    departure_time: "2025-06-05T09:15:00-07:00",
+    arrival_time: "2025-06-05T17:25:00-04:00"
+  },
+  {
+    departure_airport: "ORD",
+    arrival_airport: "DEN",
+    departure_time: "2025-06-05T11:30:00-05:00",
+    arrival_time: "2025-06-05T13:10:00-06:00"
+  },
+  {
+    departure_airport: "SEA",
+    arrival_airport: "ATL",
+    departure_time: "2025-06-05T14:05:00-07:00",
+    arrival_time: "2025-06-05T21:50:00-04:00"
+  },
+  {
+    departure_airport: "DFW",
+    arrival_airport: "PHX",
+    departure_time: "2025-06-05T17:20:00-05:00",
+    arrival_time: "2025-06-05T17:55:00-07:00"
+  },
+  {
+    departure_airport: "MCO",
+    arrival_airport: "ORD",
+    departure_time: "2025-06-06T06:50:00-04:00",
+    arrival_time: "2025-06-06T08:35:00-05:00"
+  },
+  {
+    departure_airport: "DEN",
+    arrival_airport: "CLT",
+    departure_time: "2025-06-06T09:10:00-06:00",
+    arrival_time: "2025-06-06T14:25:00-04:00"
+  },
+  {
+    departure_airport: "LAS",
+    arrival_airport: "ATL",
+    departure_time: "2025-06-06T11:45:00-07:00",
+    arrival_time: "2025-06-06T18:55:00-04:00"
+  },
+  {
+    departure_airport: "PHX",
+    arrival_airport: "LAS",
+    departure_time: "2025-06-06T15:30:00-07:00",
+    arrival_time: "2025-06-06T16:40:00-07:00"
+  },
+  {
+    departure_airport: "CLT",
+    arrival_airport: "LAX",
+    departure_time: "2025-06-06T17:55:00-04:00",
+    arrival_time: "2025-06-06T20:25:00-07:00"
   }
 ]
 
