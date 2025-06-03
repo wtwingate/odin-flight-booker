cae9fb55fb50054a452753cc04ea8d4e2d80d0f5
William Wingate <wtwingate@gmail.com>
2025-06-03 15:14:17 -0400

    Create search form for Flights index page

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,4 +7,9 @@ class Flight < ApplicationRecord
   validates :departure_time, presence: true
   validates :arrival_time, presence: true
   validates :arrival_time, comparison: { greater_than: :departure_time }
+
+  def self.dates
+    times = order(:departure_time).pluck(:departure_time)
+    times.map(&:to_date).uniq
+  end
 end
