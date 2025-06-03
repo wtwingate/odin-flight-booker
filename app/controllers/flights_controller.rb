16013db08d2e2044a312b1ff544406663f86747f
William Wingate <wtwingate@gmail.com>
2025-06-03 16:36:48 -0400

    Display Flight search results in form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,9 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @flights = @flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
+    @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+    @flights = @flights.by_date(Date.parse(params[:date])) if params[:date].present?
+    @passenger_count = params[:passenger_count].to_i || 1
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -8,6 +8,8 @@ class Flight < ApplicationRecord
   validates :arrival_time, presence: true
   validates :arrival_time, comparison: { greater_than: :departure_time }
 
+  scope :by_date, ->(date) { where(departure_time: date.beginning_of_day..date.end_of_day) }
+
   def self.dates
     times = order(:departure_time).pluck(:departure_time)
     times.map(&:to_date).uniq
