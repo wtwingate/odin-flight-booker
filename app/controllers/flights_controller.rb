a7b31065b1b43d0e53e3fe73dc9edc638668ac0d
William Wingate <wtwingate@gmail.com>
2025-06-03 23:03:40 -0400

    Use eager loading to prevent N+1 queries

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,9 +1,20 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
-    @flights = @flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
-    @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
-    @flights = @flights.by_date(Date.parse(params[:date])) if params[:date].present?
-    @passenger_count = params[:passenger_count].present? ? params[:passenger_count].to_i : 1
+    if search_params_present?
+      @flights = Flight.includes(:departure_airport, :arrival_airport)
+      @flights = @flights.where(departure_airport_id: params[:departure_airport_id]) if params[:departure_airport_id].present?
+      @flights = @flights.where(arrival_airport_id: params[:arrival_airport_id]) if params[:arrival_airport_id].present?
+      @flights = @flights.by_date(Date.parse(params[:date])) if params[:date].present?
+      @passenger_count = params[:passenger_count].present? ? params[:passenger_count].to_i : 1
+    end
+  end
+
+  private
+
+  def search_params_present?
+    params[:departure_airport_id].present? ||
+      params[:arrival_airport_id].present? ||
+      params[:date].present? ||
+      params[:passenger_count].present?
   end
 end
